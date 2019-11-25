42036fa4a204b16bb4ac845893f62ada6f8e2864
Jeremiah Isaiah <jerboygt@gmail.com>
2019-11-25 21:56:45 +0700

    Updated final build n test build

--- Final Project/Index/Index.js
+++ Final Project/Index/Index.js
@@ -5,13 +5,12 @@
 }*/
 
 //* Login Info Check
-var username = document.getElementById('usernameInput');
-var password = document.getElementById('passwordInput');
+var username = localStorage.getItem('Username');
+var password = localStorage.getItem('Password');
 
 function login() {
-    if(loginForm.username.value === "Username"){ //! PLACEHOLDER USERNAME N PASSWORD PLS CHANGE
-        if(loginForm.password.value === "Password"){
-            console.log("this works")
+    if(loginForm.usernameInput.value === username){
+        if(loginForm.passwordInput.value === password){
             myModal.style.display = "none";
         }
         else{
@@ -25,18 +24,70 @@ function login() {
 
 //* For the search bar/add button
 function addbutton() {
-    var textInput = document.getElementById('headerEntry');
-    if(textInput != '') {
-        console.log('it works')
-        //! ONLY ADDS A COPY OF TASKLABEL1 PLZ FIX
-        var new_task = document.getElementById('undoneTask1');
-        var copy = new_task.cloneNode(true);
-        document.getElementById('incompCont').appendChild(copy);
+    var label = document.getElementById('headerEntry').value
+    var newTask = document.createElement('li');
+
+    if(label !== '') {
+        // Setting the li id
+        var liid = document.createAttribute('id');
+        liid.value = 'undoneTask';
+        newTask.setAttribute('id', 'undoneTask');
+
+        // Add label
+        var labeltitle = document.createElement('label');
+        labeltitle.innerHTML = label;
+        newTask.appendChild(labeltitle);
+        var labelclass = document.createAttribute('class');
+        labelclass.value = 'taskLabel';
+        labeltitle.setAttribute('class', 'taskLabel');
+
+        // Adding checkbox button
+        var checkbutton = document.createElement('button');
+        newTask.appendChild(checkbutton);
+        var buttonid = document.createAttribute('id');
+        buttonid.value = 'checkbutton';
+        checkbutton.setAttribute('id', 'checkbutton');
+        var buttonclick = document.createAttribute('onclick');
+        buttonclick.value = 'check()';
+        checkbutton.setAttribute('onclick', 'check()');
+
+        // Adding the checkbox
+        var boximg = document.createElement('img');
+        boximg.src = "../indexTESTBUILD/Untitled-4.png";
+        checkbutton.appendChild(boximg);
+        var imgid = document.createAttribute('id');
+        imgid.value = 'checkbox';
+        boximg.setAttribute('id', 'checkbox');
+
+        // Adding the trash button
+        var trashbutton = document.createElement('button');
+        newTask.appendChild(trashbutton);
+        var tbuttonid = document.createAttribute('id');
+        tbuttonid.value = 'trashbutton';
+        trashbutton.setAttribute('id', 'trashbutton');
+        var removetask = document.createAttribute('onclick');
+        removetask.value = 'trash()';
+        trashbutton.setAttribute('onclick', 'trash()');
+
+        // Adding the trash bin icon
+        var trashbinicon = document.createElement('img');
+        trashbinicon.src = "../indexTESTBUILD/Untitled-3.png";
+        trashbutton.appendChild(trashbinicon);
+        var trashid = document.createAttribute('id');
+        trashid.value = 'heejin';
+        trashbinicon.setAttribute('id', 'heejin');
+
+        // Appending the final li
+        document.getElementById('incompCont').appendChild(newTask);
     }
 }
 
+//! FIGURE OUT HOW TO SAVE ADDED TASKS TO LOCAL STORAGE ITS A REAL PAIN THE ASS
+
+
 //* Check Button
 var x = 0;
+
 function check() {
     if (x == 0){
         document.getElementById('checkbox').src = "../indexTESTBUILD/Untitled-5.png"
@@ -45,7 +96,6 @@ function check() {
         document.getElementById('checkbox').src = "../indexTESTBUILD/Untitled-4.png"
         x = 0;
     }
-    
     console.log('check works') //!PLACEHOLDER TEXT
 }
 
@@ -56,46 +106,25 @@ function trash() { //! SO FAR THIS IS ONLY ABLE TO REMOVE THE FINISHED TASK, MAK
     
 }
 
-//* Task Detail Popup
-var modal1 = document.getElementById("Modal1");
-var taskbtn = document.getElementById("taskButton");
-
-function taskinfo() {
-    modal1.style.display = "block";
-}
-
-function closetaskdetail() {
-    modal1.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-}
-
-
 //* Dark Mode
-var x = 0;
+var x = '0';
+
 function darken() {
-    if (x == 0){
-        document.getElementById('dmicon').src = "../indexTESTBUILD/i24_photoedit_brightness-512.png"
+    if (x == '0'){
+        document.getElementById('dmicon').src = "../indexTESTBUILD/i24_photoedit_brightness-512.png";
         document.getElementById('html').style.backgroundColor = "rgb(25, 25, 25)";
         document.getElementById('incompTitle').style.color = "rgb(255, 255, 255)";
         document.getElementById('compTitle').style.color = "rgb(255, 255, 255)";
-        document.getElementById('finishedCont').style.color = "rgb(100, 100, 100)";
         document.getElementById('darkmode').style.backgroundColor = "rgb(170, 170, 170)";
         document.getElementById('headerRec').style.boxShadow = "0px 4px 6px black;";
-        x = 1;
+        x = '1';
     } else{
-        document.getElementById('dmicon').src = "../indexTESTBUILD/nightmode-512.png"
+        document.getElementById('dmicon').src = "../indexTESTBUILD/nightmode-512.png";
         document.getElementById('html').style.backgroundColor = "rgb(240, 240, 240, 0.98)";
         document.getElementById('incompTitle').style.color = "rgb(0, 0, 0)";
         document.getElementById('compTitle').style.color = "rgb(0, 0, 0)";
-        document.getElementById('finishedCont').style.color = "rgb(240, 240, 240)";
         document.getElementById('darkmode').style.backgroundColor = "rgb(255, 255, 255)";
         document.getElementById('headerRec').style.boxShadow = "0px 4px 6px black;";
-        x = 0;
-    }
-    
+        x = '0';
+    } 
 }
--- Final Project/SignUpPage/SignUpPage.js
+++ Final Project/SignUpPage/SignUpPage.js
@@ -1,17 +1,38 @@
-//* Dark Mode
-var text = document.getElementsByClassName('h1');
-function darken() {
-    document.getElementById('html').style.backgroundColor = "rgb(25, 25, 25)";
-    document.getElementById('signintext').style.color = "rgb(255, 255, 255)";
-    document.getElementById('pinfo').style.color = "rgb(255, 255, 255)";
-    document.getElementById('pwordfield').style.color = "rgb(255, 255, 255)";
-    document.getElementById('miscinfofield').style.color = "rgb(255, 255, 255)";
-    document.getElementById('darkmode').style.backgroundColor = "rgb(170, 170, 170)";
+//* Submit Field Info
+function sendinfo() {
+    var username = document.getElementById('userName').value;
+    var pword = document.getElementById('password').value;
+    var confpword = document.getElementById('confirmPassword').value;
+    if(pword === confpword) {
+        localStorage.setItem('Username', username);
+        localStorage.setItem('Password', pword);
+        console.log(localStorage)
+    } else {
+        alert('Passwords do not match! Please try again.')
+    }
 }
 
-//* Check if forms are filled
-function submit() {
-    if(document.getElementById('firstName') == ""){
-        console.log("first name")
+//* Dark Mode
+function darken() {
+    var x = 0;
+    if(x == 0) {
+        document.getElementById('dmicon').src = "../indexTESTBUILD/i24_photoedit_brightness-512.png";
+        document.getElementById('html').style.backgroundColor = "rgb(25, 25, 25)";
+        document.getElementById('why').style.color = "rgb(255, 255, 255)";
+        document.getElementById('signintext').style.color = "rgb(255, 255, 255)";
+        document.getElementById('pinfo').style.color = "rgb(255, 255, 255)";
+        document.getElementById('pwordfield').style.color = "rgb(255, 255, 255)";
+        document.getElementById('miscinfofield').style.color = "rgb(255, 255, 255)";
+        document.getElementById('darkmode').style.backgroundColor = "rgb(170, 170, 170)";
+        x = 1;
+    } else {
+        document.getElementById('dmicon').src = "../indexTESTBUILD/nightmode-512.png";
+        document.getElementById('html').style.backgroundColor = "rgb(240, 240, 240, 0.98)";
+        document.getElementById('signintext').style.color = "rgb(0, 0, 0)";
+        document.getElementById('pinfo').style.color = "rgb(0, 0, 0)";
+        document.getElementById('pwordfield').style.color = "rgb(0, 0, 0)";
+        document.getElementById('miscinfofield').style.color = "rgb(0, 0, 0)";
+        document.getElementById('darkmode').style.backgroundColor = "rgb(255, 255, 255)";
+        x = 0;
     }
 }
